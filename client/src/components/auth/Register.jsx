aee8f2039cf583a571122864c743d3fe010796ca
gota1993 <gocha_93@yahoo.com>
2018-12-14 01:29:49 +0400

    changed/added register form styles

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -46,104 +46,138 @@ export default class Register extends Component {
           value.isLogged ? (
             <Redirect to="/" />
           ) : (
-            <>
-              <h2>Register</h2>
+              <>
+                <h2 className="form-header">Register</h2>
 
-              <form
-                onSubmit={this.onSubmitHandler}
-                className="user-registration-form"
-              >
-                <div className="form-row">
-                  <div className="form-group col-md-6">
-                    <label htmlFor="inputEmail">Email</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="inputEmail"
-                      name="email"
-                      placeholder="test@email.com"
-                      value={this.state.email}
-                      onChange={this.onChangeHandler}
-                    />
-                  </div>
-                  <div className="form-group col-md-6">
-                    <label htmlFor="inlineFormInputGroupUsername">
-                      Username
+                <form
+                  onSubmit={this.onSubmitHandler}
+                  className="user-registration-form"
+                >
+                  <div className="form-row">
+                    <div className="form-group col-md-6">
+                      <label htmlFor="inputEmail">Email</label>
+                      <div className="input-group">
+                        <div className="input-group-prepend">
+                          <div className="input-group-text"><i class="fa fa-envelope fa" aria-hidden="true"></i></div>
+                        </div>
+
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="inputEmail"
+                          name="email"
+                          placeholder="test@email.com"
+                          value={this.state.email}
+                          onChange={this.onChangeHandler}
+                        />
+                      </div>
+
+                    </div>
+                    <div className="form-group col-md-6">
+                      <label htmlFor="inlineFormInputGroupUsername">
+                        Username
                     </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="inlineFormInputGroupUsername"
-                      name="username"
-                      placeholder="Username"
-                      value={this.state.username}
-                      onChange={this.onChangeHandler}
-                    />
+                      <div className="input-group">
+                        <div className="input-group-prepend">
+                          <div className="input-group-text"><i class="fa fa-user fa" aria-hidden="true"></i></div>
+                        </div>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="inlineFormInputGroupUsername"
+                          name="username"
+                          placeholder="Username"
+                          value={this.state.username}
+                          onChange={this.onChangeHandler}
+                        />
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div className="form-row">
-                  <div className="form-group col-md-6">
-                    <label htmlFor="inputPassword1">Password</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="inputPassword1"
-                      name="password"
-                      placeholder="Password"
-                      value={this.state.password}
-                      onChange={this.onChangeHandler}
-                    />
+                  <div className="form-row">
+                    <div className="form-group col-md-6">
+                      <label htmlFor="inputPassword1">Password</label>
+                      <div className="input-group">
+                        <div className="input-group-prepend">
+                          <div className="input-group-text"><i class="fa fa-lock fa" aria-hidden="true"></i></div>
+                        </div>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="inputPassword1"
+                          name="password"
+                          placeholder="Password"
+                          value={this.state.password}
+                          onChange={this.onChangeHandler}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group col-md-6">
+                      <label htmlFor="inputPassword2">Confirm Password</label>
+                      <div className="input-group">
+                        <div className="input-group-prepend">
+                          <div className="input-group-text"><i class="fa fa-lock fa" aria-hidden="true"></i></div>
+                        </div>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="inputPassword2"
+                          name="password2"
+                          placeholder="Confirm Password"
+                          value={this.state.password2}
+                          onChange={this.onChangeHandler}
+                        />
+                      </div>
+                    </div>
                   </div>
-                  <div className="form-group col-md-6">
-                    <label htmlFor="inputPassword2">Confirm Password</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="inputPassword2"
-                      name="password2"
-                      placeholder="Confirm Password"
-                      value={this.state.password2}
-                      onChange={this.onChangeHandler}
-                    />
-                  </div>
-                </div>
 
-                <div className="form-row">
-                  <div className="form-group col-md-6">
-                    <label htmlFor="formGroupExampleInput1">
-                      Date of Birth
+                  <div className="form-row">
+                    <div className="form-group col-md-6">
+                      <label htmlFor="formGroupExampleInput1">
+                        Date of Birth
                     </label>
-                    <input
-                      type="date"
-                      className="form-control"
-                      id="formGroupExampleInput1"
-                      placeholder="Date of Birth"
-                      name="birthday"
-                      value={this.state.birthday}
-                      onChange={this.onChangeHandler}
-                    />
+                      <div className="input-group">
+                        <div className="input-group-prepend">
+                          <div className="input-group-text"><i class="fas fa-calendar" aria-hidden="true"></i></div>
+                        </div>
+                        <input
+                          type="date"
+                          className="form-control"
+                          id="formGroupExampleInput1"
+                          placeholder="Date of Birth"
+                          name="birthday"
+                          value={this.state.birthday}
+                          onChange={this.onChangeHandler}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group col-md-6">
+                      <label htmlFor="formGroupExampleInput2">Balance</label>
+                      <div className="input-group">
+                        <div className="input-group-prepend">
+                          <div className="input-group-text"><i class="fas fa-dollar-sign" aria-hidden="true"></i></div>
+                        </div>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="formGroupExampleInput2"
+                          name="balance"
+                          placeholder="1000"
+                          value={this.state.balance}
+                          onChange={this.onChangeHandler}
+                        />
+                      </div>
+                    </div>
                   </div>
-                  <div className="form-group col-md-6">
-                    <label htmlFor="formGroupExampleInput2">Balance</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="formGroupExampleInput2"
-                      name="balance"
-                      placeholder="1000"
-                      value={this.state.balance}
-                      onChange={this.onChangeHandler}
-                    />
+                  
+                  <div class="col-md-12 text-center">
+                    <button type="submit" className="btn btn-primary register-button">
+                      Register
+                    </button>
                   </div>
-                </div>
-
-                <button type="submit" className="btn btn-primary">
-                  Register
-                </button>
-              </form>
-              {this.state.showError && <p className="invalid">Invalid form</p>}
-            </>
-          )
+                </form>
+                {this.state.showError && <p className="invalid">Invalid form</p>}
+              </>
+            )
         }
       </Consumer>
     );
